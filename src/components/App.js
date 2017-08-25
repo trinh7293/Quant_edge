d9e33e002cff7902efdd9daccbf5b4af1cf15294
trinh7293 <trinh7293@gmail.com>
2017-08-25 23:58:34 +0700

    fix layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import VisibleTable from '../containers/VisibleTable'
-import Header from './Header'
 import Tabs from './Tabs'
-import Tabs2 from './Tabs2'
 
 const App = () => (
   <div>
-    <Tabs2 />
+    <Tabs />
   </div>
 )
 
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import {Tabs, Tab} from 'material-ui/Tabs';
-// From https://github.com/oliviertassinari/react-swipeable-views
-import SwipeableViews from 'react-swipeable-views';
+import React from 'react'
 import VisibleTable from '../containers/VisibleTable'
+import {Tabs, Tab} from 'material-ui/Tabs'
+import { setFilter } from '../actions'
+import { jump } from '../actions'
+import { connect } from 'react-redux'
 
 const styles = {
   headline: {
@@ -11,52 +12,51 @@ const styles = {
     marginBottom: 12,
     fontWeight: 400,
   },
-  slide: {
-    padding: 10,
-  },
 };
 
-export default class TabsExampleSwipeable extends React.Component {
+class TabsExampleControlled extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      slideIndex: 0,
+      value: 'SHOW_ALL',
     };
   }
 
   handleChange = (value) => {
     this.setState({
-      slideIndex: value,
+      value: value,
     });
+    let dispatch = this.props.dispatch;
+    dispatch(setFilter(value));
   };
 
   render() {
     return (
-      <div>
-        <Tabs
-          onChange={this.handleChange}
-          value={this.state.slideIndex}
-        >
-          <Tab label="Tab One" value={0} />
-          <Tab label="Tab Two" value={1} />
-          <Tab label="Tab Three" value={2} />
-        </Tabs>
-        <SwipeableViews
-          index={this.state.slideIndex}
-          onChangeIndex={this.handleChange}
-        >
-          <div>
-            <VisibleTable />
-          </div>
-          <div style={styles.slide}>
-            <VisibleTable />
-          </div>
-          <div style={styles.slide}>
-            <VisibleTable />
-          </div>
-        </SwipeableViews>
-      </div>
+      <Tabs
+        className = 'tabs'
+        value={this.state.value}
+        onChange={this.handleChange}
+      >
+        <Tab className = 'tab' label="TOP LOOSER" value="TOP_LOOSER">
+          <VisibleTable />
+        </Tab>
+        <Tab className = 'tab' label="TOP GAINERS" value="TOP_GAINERS">
+          <VisibleTable />
+        </Tab>
+        <Tab className = 'tab' label="ALL" value="SHOW_ALL">
+          <VisibleTable />
+        </Tab>
+      </Tabs>
     );
   }
+
+  componentDidMount() {
+    let dispatch = this.props.dispatch;
+    let change = () => dispatch(jump());
+    setInterval(change, 1000)
+  }
 }
+TabsExampleControlled = connect()(TabsExampleControlled)
+
+export default TabsExampleControlled
